d6d6c8f80009e092ea93770cdf25ac397ea2422e
Nalin <nalindalal2004@gmail.com>
2025-09-20 18:37:46 +0530

    userId is unique, but anonName isn't

--- src/app/anon/set.ts
+++ src/app/anon/set.ts
@@ -5,7 +5,10 @@ const prisma = new PrismaClient();
 
 // POST /api/anon/set
 // Body: { anonName: string, userId: string }
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
