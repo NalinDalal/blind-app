0adacd53d2c8993e5c7c5861bd5a7e23176eb005
Nalin <nalindalal2004@gmail.com>
2025-09-21 13:02:26 +0530

    migration for comment like

---
+++ prisma/migrations/20250921073203_comment_like/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[userId]` on the table `AnonMapping` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[anonName]` on the table `AnonMapping` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateTable
+CREATE TABLE "public"."CommentLike" (
+    "id" TEXT NOT NULL,
+    "commentId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CommentLike_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CommentLike_commentId_userId_key" ON "public"."CommentLike"("commentId", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AnonMapping_userId_key" ON "public"."AnonMapping"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AnonMapping_anonName_key" ON "public"."AnonMapping"("anonName");
+
+-- AddForeignKey
+ALTER TABLE "public"."CommentLike" ADD CONSTRAINT "CommentLike_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "public"."Comment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."CommentLike" ADD CONSTRAINT "CommentLike_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
