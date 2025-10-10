f9e26c0080ce763ffe063af6708b68f8a4affcba
Nalin <nalindalal2004@gmail.com>
2025-10-10 13:19:14 +0530

    test depreciation resolved

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,16 @@
+/** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/src", "<rootDir>/test"],
   moduleFileExtensions: ["ts", "js", "json"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.json" }],
   },
   testMatch: ["**/?(*.)+(test).[tj]s", "**/test/**/*.test.[tj]s"],
   collectCoverageFrom: ["src/**/*.{ts,js}", "!src/**/*.d.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "^~/(.*)$": "<rootDir>/$1",
-  },
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.json",
-    },
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
