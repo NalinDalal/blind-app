f5958c5ed35ff9029f82b5a1c3cc061da3ad429d
Nalin <nalindalal2004@gmail.com>
2025-12-15 01:05:30 +0530

    fix: move to new prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@
  * to prevent connection pool exhaustion in development.
  * @module lib/prisma
  */
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 /**
  * Global object augmentation for Prisma Client.
